9ed6ef938b8b9507a2e6d25976c63dd184a709e3
Anjalkhadka <85629558+Anjalkhadka@users.noreply.github.com>
2024-01-13 09:27:33 +0545

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,19 +4,6 @@ const jwt = require("jsonwebtoken");
 const User = require("../model/user");
 const Shop = require("../model/shop");
 
-exports.isAuthenticated = catchAsyncErrors(async(req,res,next) => {
-    const {token} = req.cookies;
-
-    if(!token){
-        return next(new ErrorHandler("Please login to continue", 401));
-    }
-
-    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-
-    req.user = await User.findById(decoded.id);
-
-    next();
-});
 
 
 exports.isSeller = catchAsyncErrors(async(req,res,next) => {
